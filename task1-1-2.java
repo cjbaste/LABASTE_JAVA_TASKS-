0405989300c98b46f8f57cc4cb4110a497822f3f
cjbaste <93541688+cjbaste@users.noreply.github.com>
2025-07-01 07:46:04 +0000

    used compound operators

--- task1-1-2.java
+++ task1-1-2.java
@@ -1,1 +1,25 @@
+class ArithmeticTask {
 
+    public static void main (String[] args){
+          
+
+
+        int result = 1 + 2; // result is now 3
+
+        System.out.println(result);
+
+        result -= 1;                // result = 2
+        System.out.println(result);
+
+        result *= 2;                // result = 4
+        System.out.println(result);
+
+        result /= 2;                // result = 2
+        System.out.println(result);
+
+        result += 8;                // result = 10
+        result %= 7;                // result = 3
+        System.out.println(result);
+    
+    }
+}
